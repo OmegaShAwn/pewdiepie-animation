4f887131725b71a4ef915d74bd8a61b48554e1e0
Shawn S. Thottan <shawn.thottan@freshtohome.com>
2020-02-02 00:37:37 +0530

    reduced swing

--- pdp.js
+++ pdp.js
@@ -7,40 +7,23 @@ const init = () => {
     svg.clear();
     svg.node.setAttribute("style","background: #fd0037")
     draw = svg.addTo('body').size(w, h);
+    wc = w/250;
+    let waves = '';
+    for (let i = 0; i < wc-2; i++) waves = waves.concat(`t ${w/wc} ${h/wc} `);
     for (let i = -1.5*h; i < 1.5*h; i += 80) {
         draw.fill({ opacity: 0 })
             .stroke({ color: 'black', width: 5 })
-            .path(`M -5 ${i} ` +
-                `q ${w/10} ${-h}, ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${(w/5)+10} ${(h/5)+10}`)
+            .path(`M -5 ${i} q ${w/10} ${-h}, ${w/wc} ${h/wc} ${waves} t ${(w/wc)+10} ${(h/wc)+10}`)
             .animate(8000)
             .ease(pos => -.5*(Math.cos(Math.PI*pos)-1))
-            .plot(`M -5 ${i} ` +
-                `q ${w/10} ${h}, ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${(w/5)+10} ${(h/5)+10}`)
+            .plot(`M -5 ${i} q ${w/10} ${-h/2}, ${w/wc} ${h/wc} ${waves} t ${(w/wc)+10} ${(h/wc)+10}`)
             .loop(true, true);
         draw.fill({ opacity: 0 })
             .stroke({ color: 'black', width: 5 })
-            .path(`M -5 ${i+5} ` +
-                `q ${w/10} ${-h}, ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${(w/5)+10} ${(h/5)+10}`)
+            .path(`M -5 ${i+5} q ${w/10} ${-h}, ${w/wc} ${h/wc} ${waves} t ${(w/wc)+10} ${(h/wc)+10}`)
             .animate(8000)
             .ease(pos => -.5*(Math.cos(Math.PI*pos)-1))
-            .plot(`M -5 ${i} ` +
-                `q ${w/10} ${h}, ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${w/5} ${h/5} ` +
-                `t ${(w/5)+10} ${(h/5)+10}`)
+            .plot(`M -5 ${i+5} q ${w/10} ${-h/2}, ${w/wc} ${h/wc} ${waves} t ${(w/wc)+10} ${(h/wc)+10}`)
             .loop(true, true);
     }
 };
